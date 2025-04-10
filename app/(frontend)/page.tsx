019850d6a04f8c96a425ab1e79a546a089689786
Maksym Petriv <petriv050711@gmail.com>
2025-04-11 01:42:53 +0200

    Init

---
+++ app/(frontend)/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home () {
+  return (
+    <div>
+      <h1>Welcome to Payload CMS</h1>
+      <p>This is a sample application using Payload CMS.</p>
+    </div>
+  );
+}
